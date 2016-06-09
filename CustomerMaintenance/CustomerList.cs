f03e4fb24c492f38ace862783c21bcb5c5f2064e
JGarnick <garnick.josh@gmail.com>
2016-06-09 16:04:19 -0700

    C# example 1
    Allows adding and removing customers from a simulated database using XML
    instead of SQL and does this using 2 forms.

---
+++ CustomerMaintenance/CustomerList.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerMaintenance
+{
+    public class CustomerList
+	{
+		private List<Customer> customers;
+
+        // declare the delegate and then the event here
+        public delegate void ChangeHandler(CustomerList customers);
+        public event ChangeHandler Changed;
+
+		public CustomerList()
+		{
+            customers = new List<Customer>();
+		}
+
+		public int Count
+		{
+			get
+			{
+				return customers.Count;
+			}
+		}
+
+		public Customer this[int i]
+		{
+			get
+			{
+				return customers[i];
+			}
+			set
+			{
+				customers[i] = value;
+				//raise the event here
+                Changed(this);
+			}
+		}
+
+		public void Fill()
+		{
+			customers = CustomerDB.GetCustomers();
+		}
+
+		public void Save()
+		{
+			CustomerDB.SaveCustomers(customers);
+		}
+
+		public void Add(Customer customer)
+		{
+			customers.Add(customer);
+			// raise the event here
+            Changed(this);
+		}
+
+		public void Remove(Customer customer)
+		{
+			customers.Remove(customer);
+			// raise the event here
+            Changed(this);
+		}
+
+		public static CustomerList operator + (CustomerList c1, Customer c)
+		{
+			c1.Add(c);
+			return c1;
+		}
+
+		public static CustomerList operator - (CustomerList c1, Customer c)
+		{
+			c1.Remove(c);
+			return c1;
+		}
+
+	}
+}
